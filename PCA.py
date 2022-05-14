05028cc38e3502354dd4a1d03b5a84b22ba912d2
Max Deng <mxd1402@g.rit.edu>
2022-05-13 21:16:24 -0400

    file changes

--- PCA.py
+++ PCA.py
@@ -2,11 +2,12 @@ from random import random
 import numpy as np #numpy import
 ###utilizing sklearn for implemented PCA/sPCA algorithms###
 from sklearn.decomposition import PCA 
-from sklearn.decomposition import SparsePCA
+from sklearn.decomposition import TruncatedSVD
+from scipy.sparse import csr_matrix
 import csv #utilizing csv reader, may change for other file formats.
 
 #A simple python package I/O based csv reader for raw input data (see contents of arbitrary.csv)
-def simplefileReader(filename): 
+def simplefileReader(filename):
     data_arr = [] #initializing array for use in PCA algorithms
     with open(filename) as file: #opening file
         dlist = csv.reader(file) #reading file line by line
@@ -17,9 +18,18 @@ def simplefileReader(filename):
 
 
 if __name__ == '__main__': #file main
-    sdata = simplefileReader("/Users/maxdeng/Documents/mxd_Gerstein/Sparsity_Control/arbitrary.csv")
-    pca = PCA(n_components=2)
-    pca.fit(sdata)
+    #data = simplefileReader("/Users/maxdeng/Documents/mxd_Gerstein/Sparsity_Control/arbitrary.csv")
+
+    np.random.seed(0)
+    data_dense = np.random.rand(100,100)
+    data_dense [:, 2 * np.arange(50)] = 0
+    data_sparse = csr_matrix(data_dense)
+    svd = TruncatedSVD(n_components=5, n_iter=7, random_state=42)
+    svd.fit(data_sparse)
+
+    pca = PCA(n_components=5, n_iter=7, random_state=42)
+    pca.fit(data_dense)
+
     print("PCA explained variance ratios:")
     print(pca.explained_variance_ratio_)
     print("PCA singular values:")
